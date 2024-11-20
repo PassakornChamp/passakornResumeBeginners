dc009dd4eb1aacfafef61666ce7c7a2e1e3d34ce
PassakornChamp <65725251+PassakornChamp@users.noreply.github.com>
2024-11-21 01:44:23 +0700

    Add Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./sections/Hero/Hero";
 import Project from "./sections/Project/Project";
+import Contact from "./sections/Contact/Contact";
 
 function App() {
   return (
     <div>
       <Hero />
       <Project />
+      <Contact />
     </div>
   );
 }
